c25d1b405d6f3750ab7b43207f4af3769aca47f8
KAESH1 <mathys971i22@gmail.com>
2021-10-08 23:25:58 -0400

    Rôle/MAIN.py-Encore
    Ajout d'un fichier et définition du code dans MAIN.py
    ~Suppression dossier pas utiles

--- MAIN.py
+++ MAIN.py
@@ -1,5 +1,40 @@
-import pygame
-
 #Code principal jeu
-#testjjkjk
-#modifier par tidiane
+import pygame
+import random
+import time
+
+# Initialisation Jeu
+pygame.init()
+
+# Nom du jeu
+pygame.display.set_caption("L'épopée_de_Lynk.exe")
+
+# Définition fenêtre (à changer si besoin)
+rés=(840,550)
+fenetre=pygame.display.set_mode(rés)
+
+# Appel horloge
+temps=pygame.time.Clock()
+
+i=0
+Play=True
+while Play:
+
+	temps.tick(30)
+	print(i)
+	i+=1
+
+	# Lecture clavier
+	touches=pygame.key.get_pressed();
+
+	if touches[pygame.K_ESCAPE]: # Échap / Quitter
+		Play=False
+	for event in pygame.event.get():
+		if event.type==pygame.QUIT:
+			Play=False
+
+	pass
+
+# Fin programme
+pygame.quit()
+quit()
