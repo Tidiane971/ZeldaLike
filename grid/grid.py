de8a1c69e380f2a89cc7417069ffcd5a3290184e
Tidiane <nicekun971@gmail.com>
2021-11-13 09:10:28 -0400

    Grid

---
+++ grid/grid.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+im = Image.open('bongrid.png')
+pix = im.load()
+
+
+T = []
+
+x,y = im.size
+for k in range(y//64):
+    T.append([])
+j=0
+for k in range(0,y,64):
+    for i in range(0,x,64):
+        if pix[i+32,k+32] == (255,0,0):
+            T[k//64].append(1)
+        else:
+            T[k//64].append(0)
+print(T)
