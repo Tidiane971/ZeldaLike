f52fd027fceb794b0db071c8b9ddc39a6b9a65bf
KAESH1 <mathys971i22@gmail.com>
2021-11-18 10:48:54 -0400

    Modif Film
    Intérruption du film

--- Film.py
+++ Film.py
@@ -2,9 +2,21 @@ from moviepy.editor import *
 import pygame
 
 visionage=True
-film = VideoFileClip('Film.mp4')
-film.preview()
 
-visionage=False
+film = VideoFileClip('Film.mp4')
+
+while visionage:
+
+    #Skip
+    if touches[pygame.K_ESCAPE]:
+        visionage=False
+    else:
+        film.preview()
+
+	#Quitter Jeu
+    touches=pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            visionage=False
 
 pygame.quit()
