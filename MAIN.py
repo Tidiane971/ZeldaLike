75baa2bd6cde851ded91beabd2f0aa0627ace391
vroum-vroum <92180616+vroum-vroum@users.noreply.github.com>
2021-11-19 18:51:24 -0400

    ajout img korogu dans la liste d'img + un peu de code pour sa création

--- MAIN.py
+++ MAIN.py
@@ -47,7 +47,7 @@ perso = perso(objet["Lynk"],fenetre,x=152,y=243,camerax=CameraX,cameray=CameraY,
 #Image curseur
 Choix=elementgraphique(objet["select"],fenetre,x=190,y=400)
 
-
+korogu_tab=[]
 #----------VARIABLES UTILES
 i=0
 poz=True
@@ -214,6 +214,13 @@ while Play:
 		perso.open(COFFRES = coffre_liste)
 
 		perso.inventaire.afficher()
+
+		#gestion korogu
+		#if i%1000=0:
+		#	korogu = ennemi(objet["ennemi"][korogu],fenetre,x=152,y=243,)
+		#	korogu_tab.append(korogu)
+		#for i in korogu_tab:
+			#i.afficher()
 
 		#Gestion coeurs
 		for w in range(4):
